ea74a76de2732e8059ad16e850e6a16447450221
ChihYuChiang <ckccyu@gmail.com>
2017-04-08 18:21:41 -0500

    Update dict extraction

--- bin/1-1_dic_extraction.py
+++ bin/1-1_dic_extraction.py
@@ -1,32 +1,48 @@
 #%%
+#--Setting up
 import re
 import pandas as pd
 
-f = open(r"D:\OneDrive\GitHub\content_analysis_final\reference\Webster's Unabridged 09\29765-8.txt")
+#Read in dict file (113,632 words)
+f = open(r"D:\OneDrive\GitHub\MACS30200proj\reference\Webster's Unabridged 09\29765-8.txt")
 text = f.read()
 
-#%%
-results_feel = re.findall('\n[^a-z]+\n.*?[f|F]eel', text, re.DOTALL)
-results_emotion = re.findall('\n[^a-z]+\n.*?[e|E]motion', text, re.DOTALL)
-results_experience = re.findall('\n[^a-z]+\n.*?[e|E]xperience', text, re.DOTALL)
-
-result_feel = set([(re.findall('\n[^a-z]+\n', s)[-1]).strip() for s in results_feel])
-result_emotion = set([(re.findall('\n[^a-z]+\n', s)[-1]).strip() for s in results_emotion])
-result_experience = set([(re.findall('\n[^a-z]+\n', s)[-1]).strip() for s in results_experience])
 
 #%%
-results_encounter = re.findall('\n[^a-z]+\n.*?[e|E]ncounter', text, re.DOTALL)
-result_encounter = set([(re.findall('\n[^a-z]+\n', s)[-1]).strip() for s in results_encounter])
-# print(result_encounter)
+#--Acquire keywords from each search term
+#sens = sensation + sense
+#perce = percept + perceive
+terms = ['feel', 'emotion', 'experience', 'encounter', 'aware', 'mind', 'sens', 'state', 'perce', 'discover', 'view', 'interest', 'physic', 'mental', 'spiritual', 'concern', 'thought', 'concept', 'belie', 'rational', 'social', 'imagin'
+]
+
+regs = ['\n[^a-z]+\n.*?[' + term[0] + '|' + term[0].upper() + ']' + term[1:] for term in terms]
+results = [re.findall(reg, text, re.DOTALL) for reg in regs]
+words = [(re.findall('\n[^a-z]+\n', s)[-1]).strip() for result in results for s in result]
+len(words)
 
 #%%
-results_sensation = re.findall('\n[^a-z]+\n.*?[s|S]ensation', text, re.DOTALL)
-result_sensation = set([(re.findall('\n[^a-z]+\n', s)[-1]).strip() for s in results_sensation])
-# print(result_sensation)
+#Remove duplicate
+keywords = pd.Series(words).drop_duplicates()
+len(keywords)
 
 #%%
-len(result_emotion | result_feel | result_experience | result_encounter | result_sensation)
-
-keywords = pd.Series(list(result_emotion | result_feel | result_experience | result_encounter | result_sensation))
-
+#Export
 keywords.to_csv(r'..\data\process\keywords.csv', index=False)
+
+
+#%%
+#--Exploration
+#Search specific term
+term = 'cogniti'
+
+reg = '\n[^a-z]+\n.*?[' + term[0] + '|' + term[0].upper() + ']' + term[1:]
+results_ex = re.findall(reg, text, re.DOTALL)
+result_ex = set([(re.findall('\n[^a-z]+\n', s)[-1]).strip() for s in results_ex])
+print(result_ex)
+print(len(result_ex))
+
+#%%
+#Search if specific keyword is in full result
+key = 'ZOMBIE'
+
+print(key in list(keywords))
