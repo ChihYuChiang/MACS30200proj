145433d3ccfeb6d6327be9fbd9e6cd32d8f7a8cb
ChihYuChiang <ckccyu@gmail.com>
2017-04-26 00:14:23 -0500

    Add comment for triplet survey; randomize selected game order

--- bin/Survey_tripet_comparison_for_qualtrics.js
+++ bin/Survey_tripet_comparison_for_qualtrics.js
@@ -1,9 +1,11 @@
-Qualtrics.SurveyEngine.addOnload(function () {
-	/*Place Your JavaScript Here*/
+Qualtrics.SurveyEngine.addOnload(function (){
 	
-	//Onclick answer
+	//--Hide proceed button
 	this.hideNextButton();
-	this.questionclick = function(event, element){
+
+
+	//--Onclick answer, click next page button, with a small delay
+	this.questionclick = function (event, element){
         //for a single answer multiple choice question, the element type will be radio
         if (element.type == 'radio')
         {	
@@ -12,29 +14,56 @@ Qualtrics.SurveyEngine.addOnload(function () {
         }
     }
 
-	var start;
-	var gap;
-	var index;
+
+	//--(kk) The looped game from loop field
 	var kk = "${lm://Field/1}";
+	//Remove space and line break
 	kk = kk.trim();
+
+
+	//--(kk1) Get chosen games from embedded data
 	var arr = "${e://Field/playedGames}";
-	console.log("arr", arr);
 	var arr1 = arr.toString();
-	//console.log("arr1",arr1);
 	var kk1 = arr1.split(",");
+
+
+	//--Shuffle chosen game order if this is the first triplet
+	function shuffle(a){
+		var j, x, i;
+		for (i = a.length; i; i--){
+			j = Math.floor(Math.random() * i);
+			x = a[i - 1];
+			a[i - 1] = a[j];
+			a[j] = x;
+		}
+	}
+
+	//Acquire current triplet count
+	var times = "${e://Field/tripletTimes}";
+	times = parseInt(times); //Convert str to int
+	if (times == 1){
+		shuffle(kk1);
+		Qualtrics.SurveyEngine.addEmbeddedData("playedGames", kk1.toString());
+	}
+
+
+	//--Decide current game index within the chosen games
+	var index;
 	var array = [];
-	var times = "${e://Field/tripletTimes}"
-	for (var i = 0; i < kk1.length; i++) {
-		if (kk1[i].trim() == kk) {
+	for (var i = 0; i < kk1.length; i++){
+		if (kk1[i].trim() == kk){
 			index = i;
 		}
+		//Also tidy up the chosen game list
 		array.push(kk1[i].trim());
 	}
-	console.log(array);
+
+
+	//--Compute the games to be compared
+	var start;
+	var gap;
 	if (index > -1) {
 		var forSwitch;
-		times = parseInt(times);
-		//console.log("times="+times);
 		if (times <= 5) {
 			forSwitch = 1;
 		} else if (times <= 10) {
@@ -74,18 +103,23 @@ Qualtrics.SurveyEngine.addOnload(function () {
 				gap = (index + 4) % 5;
 				break;
 		}
-		console.log(array[index] + ", " + array[start] + ", " + array[gap]);
 
+
+		//--Get choice obj and Set choice text
 		var choices = document.getElementsByClassName('SingleAnswer');
 		choices[choices.length - 2].innerHTML = array[start];
 		choices[choices.length - 1].innerHTML = array[gap];
 	}
 	
+
+	//--Record what games are compared in each triplet
 	Qualtrics.SurveyEngine.addEmbeddedData(
 		"triplet" + times,
 		this.getQuestionInfo().QuestionID + ", " + array[index] + ", " + array[start] + ", " + array[gap]
 	);
 
+
+	//--Counting triplet time
 	Qualtrics.SurveyEngine.addEmbeddedData("tripletTimes", times + 1);
 
 });
