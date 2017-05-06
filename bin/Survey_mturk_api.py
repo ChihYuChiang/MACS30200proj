2bf240a9f3400dd1c91063d2038cd65f3ddbdc06
ChihYuChiang <ckccyu@gmail.com>
2017-05-06 13:12:12 -0500

    Update mturk api usage

--- bin/Survey_mturk_api.py
+++ bin/Survey_mturk_api.py
@@ -1,3 +1,4 @@
+#%%
 #--Setting up
 import time
 import boto3 #AWS mturk API access
@@ -5,10 +6,64 @@ import boto3 #AWS mturk API access
 mturk = boto3.client('mturk')
 
 
+
+
+'''
+------------------------------
+Block worker
+------------------------------
+'''
+#%%
+response = mturk.create_worker_block(
+    WorkerId='A3DS068PGQ5RBN',
+    Reason='As stated in the previous email.'
+)
+print(response)
+
+
+
+
+'''
+------------------------------
+Review assignments (in progress)
+------------------------------
+'''
+#%%
+#--Acquire HIT info
+#Status='Reviewable'|'Reviewing'
+response = mturk.list_reviewable_hits(
+    HITTypeId='3MHB1T0JXN7YQQHPEMXFW8J0CHKJS7',
+    Status='Reviewable',
+    MaxResults=100
+)
+response['HITs']
+
+
+#%%
+#--Acquire assignment info
+#AssignmentStatuses=['Submitted'|'Approved'|'Rejected']
+response = mturk.list_assignments_for_hit(
+    HITId='3ICOHX7ENCB7OA23YRXN5680YJRE09',
+    MaxResults=100,
+    AssignmentStatuses=[
+        'Submitted', 'Approved','Rejected',
+    ]
+)
+response['Assignments']
+
+
+
+
+'''
+------------------------------
+Create new HIT by specified time gap
+------------------------------
+'''
+#%%
 #--Configure
-totalCount = 16 #Total HITs completed (for request token)
-batch = 3 #Number of HITs to be automated in a batch
-gap = 3 * 60 * 60 #Gap hours between each HIT
+totalCount = 39 #Total HITs completed (for request token)
+batch = 2 #Number of HITs to be automated in a batch
+gap = int(3.5 * 60 * 60) #Gap hours between each HIT
 
 
 #--Define function for recursion
