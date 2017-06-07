b9d18b1ee573fae712ee44e7517c4cb15386a39d
ChihYuChiang <ckccyu@gmail.com>
2017-06-07 01:44:15 -0500

    Add minmax comment

--- bin/Survey_shun_detect_max&min.js
+++ bin/Survey_shun_detect_max&min.js
@@ -1,8 +1,11 @@
 Qualtrics.SurveyEngine.addOnload(function () {
 	/*Place Your JavaScript Here*/
-    var Q_TARGET = 1;
-    var MIN_NO = 2;
-    var MIN_THR = 15;
+    //--Parameters
+    var Q_TARGET = 1; //The order of question of that page
+    var MIN_NO = 2; //Retain n minimum value
+    var MIN_THR = 15; //Only retain minimum value smaller than it
+
+    //Acquire answer value and item text from previous question
     var values = [
         "${q://QID1/ChoiceNumericEntryValue/1}",
         "${q://QID1/ChoiceNumericEntryValue/2}",
@@ -15,17 +18,23 @@ Qualtrics.SurveyEngine.addOnload(function () {
         "${q://QID1/ChoiceDescription/3}",
         "${q://QID1/ChoiceDescription/4}",
         "${q://QID1/ChoiceDescription/5}"];
+    
 
+    //--Item prototype
     function Item(text, value) {
         this.text = text;
         this.value = value;
     }
 
+    //Initiate each item
     var items = [];
     for(i = 0; i < values.length; i++) {
         items[i] = new Item(texts[i], values[i]);
     }
 
+
+    //--Acquire min (smallest n values)
+    //Sort item by answer value
     items.sort(function(a, b){return a.value - b.value});
     mins = [];
     for(i = 0; i < MIN_NO; i++) {
@@ -33,13 +42,17 @@ Qualtrics.SurveyEngine.addOnload(function () {
         mins[i] = items[i];
     }
 
+
+    //--Acquire max
     items.reverse();
     max = items[0];
 
+
+    //--Forge text to replace question text
     textReplaces = [];
     textReplaces[0] = "The max option is " + max.text;
     switch(mins[0]) {
-        case undefined:
+        case undefined: //If the min values are filtered out then do not show
             textReplaces[1] = "";
             break;
         default:
@@ -53,11 +66,14 @@ Qualtrics.SurveyEngine.addOnload(function () {
             textReplaces[2] = "<br>The second min option is " + mins[1].text;
     }
 
+    //Concatenate the prepared texts
     var textReplace = ""
     for(i = 0; i < textReplaces.length; i++) {
         textReplace += textReplaces[i];
     }
 
+
+    //--Impute the prepared texts as the question text
     var questionText = document.getElementsByClassName('QuestionText');
     questionText[Q_TARGET - 1].innerHTML = textReplace;
 });
