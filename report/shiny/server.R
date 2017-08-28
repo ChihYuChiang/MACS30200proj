ddc2972d5b83386256bec154807a62000e2cab3d
ChihYuChiang <ckccyu@gmail.com>
2017-08-28 03:20:17 -0500

    Update typography

--- report/shiny/server.R
+++ report/shiny/server.R
@@ -9,6 +9,7 @@ library(DT)
 library(tidyverse)
 library(scales)
 library(data.table)
+library(feather)
 
 #Run app
 # runApp("../report/shiny")
@@ -33,20 +34,30 @@ df_main <- fread("data/df_cb_main.csv", header=TRUE) %>%
   filter(CoreID == 0) %>% #Only non-core game
   select(-`Short Description`, -Source, -CoreID, -Predicted, -`Author Name`, -`Review`, -`File Name`) %>% #Remove uncessary columns
   distinct(`Game Title`, .keep_all=TRUE) %>% #Keep one game only one entry
-  mutate_at(vars(matches("^[0-9]+$")), funs(round(., digits=4))) #Round score columns for later % display
-
+  mutate_at(vars(matches("^[0-9]+$")), funs(round(., digits=4))) %>% #Round score columns for later % display
+  select(`Game Title`, `Release Date`, `ESRB`, `GS Score`, `User Score`, everything()) #Reorder columns for later display
+names(df_main) <- gsub("^([0-9]+)$", "Genre \\1", names(df_main)) #Rename genre scores
 
 #--Acquire distance matrix (based on the 7 genre scores)
-df_dist <- select(df_main, num_range("", c(1:7))) %>%
-  dist(method="euclidean", diag=FALSE, upper=FALSE) %>% #Euclidean is enough while the genre classification was based on Cosine between games
-  as.matrix()
+RETRAIN <- FALSE
+if(RETRAIN) {
+  df_dist <- select(df_main, starts_with("Genre")) %>%
+    dist(method="euclidean", diag=FALSE, upper=FALSE) %>% #Euclidean is enough while the genre classification was based on Cosine between games
+    as.matrix() %>% #Convert a sparce matrix into dense
+    saveRDS("data/df_dist.rds")
+}
+df_dist <- readRDS("data/df_dist.rds")
 
 
 #--Read in scores of each keyword group; normalization (for identifying the differentiating features)
 #../data/process/score_300_doc2vec.csv
-df_keywordScore <- fread("data/score_300_doc2vec.csv", header=TRUE) %>%
-  mutate_at(vars(starts_with("group")), funs(scale(.))) %>% #New vars with a "st" suffix
-  as.data.table() #Coerce back to dt to avoid error
+READCSV <- FALSE
+if(READCSV) {
+  df_keywordScore <- fread("data/score_300_doc2vec.csv", header=TRUE) %>%
+    mutate_at(vars(starts_with("group")), funs(st = scale(.))) %>% #New vars with a "st" suffix
+    write_feather("data/score_300_doc2vec.feather") #Save feather to improve processing speed
+}
+df_keywordScore <- read_feather("data/score_300_doc2vec.feather") %>% as.data.table()
 
 
 #--Read in keyword group terms and create a keyword dic
@@ -126,44 +137,110 @@ server <- function(input, output) {
   
   
   #--Acquire similar games
+  #Based on 7 genre scores
   similarTitleTb.out <- reactive({
+    #A shell df for display when no target games selected
     if(is.null(input$searchResult_rows_selected)){
       return(searchResultTb.out()[input$searchResult_rows_selected, ])
     }
+    
+    #Acquire target title name from selection
     targetTitle <- searchResultTb.out()[[input$searchResult_rows_selected, "Game Title"]]
+    
+    #Search the title in main
     targetTitleIndex <- match(targetTitle, df_main[["Game Title"]])
-    similarTitleIndice <- sort(df_dist[targetTitleIndex, ])[2:6] %>%
-      names() %>%
+    
+    #Use the target's index to find similar titles (with smallest dist) in df_dist
+    similarTitleIndice <- sort(df_dist[ ,1])[2:6] %>% #Index 1 will be the target game itself
+      names() %>% #Acquire their indices
       as.numeric()
+    
+    #Use the similar titles' indices to find similar titles in main
     df_main[similarTitleIndice, ]
   })
   
-  
-  #--Acquire distinguishing attributes
-  distingushingKeyTb.out <- reactive({
-    if(is.null(input$searchResult_rows_selected)){
-      return(data.table(Abs = character(), V1 = character(), Percentage = character()))
-    }
-    targetTitle <- searchResultTb.out()[[input$searchResult_rows_selected, "Game Title"]]
 
-    targetKeyScores <- filter(df_keywordScore, Game == targetTitle) %>%
+  #--Acquire dominant attributes
+  dominantKeyTb.out <- reactive({
+    #A shell df for display when no target games selected (the structure has to be compatible to the operations in rendering)
+    if(is.null(input$searchResult_rows_selected)){
+      return(data.table(Similarity.abs = character(), Similarity = character(), `Percent Rank` = character()))
+    }
+    
+    #Acquire target title name from selection
+    targetTitle <- searchResultTb.out()[[input$searchResult_rows_selected, "Game Title"]]
+    
+    #Acquire target's scores (unstanderdized) on each keyword group
+    targetKeyScores <- df_keywordScore[Game == targetTitle][1] %>% #Limit to first matched entry if there are multiple
       select(matches("^group[0-9]+$")) %>%
-      t() %>%
-      as.data.table(keep.rownames="keygroup") %>%
-      mutate(Abs = abs(V1)) %>%
-      top_n(5)
+      t() %>% #Transpose
+      as.data.table(keep.rownames="keygroup") %>% #Transform rownames (group123) into one column
+      setnames("V1", "score") %>% #Reset column name
+      setkey(score) %>% #Set key for sorting
+      .[order(-score)] %>% #Filter for top 5 absolute score (so that the list include the distinguishing features, comparatively high or low)
+      .[1:5]
     
-    x <- map(.x=targetKeyScores$keygroup, .f= ~ ecdf(df_keywordScore[[.x]]))
-    y <- map2(.x=x, .y=targetKeyScores$V1, .f= ~ .x(.y))
-      
-    targetKeyScores$Percentage <- unlist(y)
-    
+    #To get the corresponding terms, filter `df_keywordDict` by the keyword group ids, cleaned from `group123`
     targetKeyScores$keywords <- map(.x=targetKeyScores$keygroup,
-                                    .f= ~ filter(df_keywordDict, cluster == as.numeric(sub(pattern="group", replacement="", x=.x)))$keyword %>% unlist())
+                                    .f= ~ df_keywordDict[cluster == as.numeric(sub(pattern="group", replacement="", x=.x))]$keyword %>%
+                                      unlist() %>%
+                                      paste(collapse=", ") #Adjust typography
+                                    )
+    
+    #Adjustments for display
+    targetKeyScores[, `:=`(score = round(score, digits=2), #Round score
+                           keygroup = sub(pattern="group", replacement="G", keygroup))] #Make cleaned name `GXXX`
+    names(targetKeyScores) <- c("Keyword Group", "Similarity", "Keywords") #Rename
+
+    #Return df
     targetKeyScores
   })
-
   
+
+  #--Acquire distinguishing attributes
+  distingushingKeyTb.out <- reactive({
+    #A shell df for display when no target games selected (the structure has to be compatible to the operations in rendering)
+    if(is.null(input$searchResult_rows_selected)){
+      return(data.table(Similarity.abs = character(), Similarity = character(), `Percent Rank` = character()))
+    }
+    
+    #Acquire target title name from selection
+    targetTitle <- searchResultTb.out()[[input$searchResult_rows_selected, "Game Title"]]
+
+    #Acquire target's scores (standerdized) on each keyword group
+    targetKeyScores.st <- df_keywordScore[Game == targetTitle][1] %>% #Limit to first matched entry if there are multiple
+      select(matches("^group[0-9]+_st$")) %>%
+      t() %>% #Transpose
+      as.data.table(keep.rownames="keygroup") %>% #Transform rownames (group123) into one column
+      setnames("V1", "score") %>% #Reset column name
+      .[ ,score.abs := abs(score)] %>% #Acquire absolute score for ranking
+      setkey(score.abs) %>% #Set key for sorting
+      .[order(-score.abs)] %>% #Filter for top 5 absolute score (so that the list include the distinguishing features, comparatively high or low)
+      .[1:5]
+      
+    x <- map(.x=targetKeyScores.st$keygroup, .f= ~ ecdf(df_keywordScore[[.x]])) #Acquire cdf of each keyword score column
+    y <- map2(.x=x, .y=targetKeyScores.st$score, .f= ~ .x(.y)) #Acquire percentage (position) of the scores of the target title by plugging in the scores into the cdfs
+    
+    #Combined into the df
+    targetKeyScores.st$percentage <- unlist(y)
+    
+    #To get the corresponding terms, filter `df_keywordDict` by the keyword group ids, cleaned from `group123`
+    targetKeyScores.st$keywords <- map(.x=targetKeyScores.st$keygroup,
+                                       .f= ~ df_keywordDict[cluster == as.numeric(sub(pattern="group([0-9]+)_st", replacement="\\1", x=.x))]$keyword %>%
+                                         unlist() %>%
+                                         paste(collapse=", ") #Adjust typography
+                                       )
+    
+    #Adjustments for display
+    targetKeyScores.st[, `:=`(percentage = round(percentage, digits=4), #Round score
+                           keygroup = sub(pattern="^group([0-9]+)_st$", replacement="G\\1", keygroup))]
+    names(targetKeyScores.st) <- c("Keyword Group", "Similarity", "Similarity.abs", "Percent Rank", "Keywords") #Rename
+    
+    #Return df
+    targetKeyScores.st
+  })
+
+    
   #--Clear data table selection 
   observeEvent(input$clearSelection, {
     dataTableProxy('queryResult') %>% selectRows(NULL)
@@ -180,75 +257,81 @@ server <- function(input, output) {
   output$searchResult <- DT::renderDataTable({
     DT::datatable(searchResultTb.out() %>%
                     mutate(` ` = `Game Title`) %>%
-                    select(`Game Title`, ` `), #Display only title and review link
+                    select(`Game Title`, `Release Date`, ` `), #Display only title, release date, and review link
                   selection="single",
-                  options=list(pageLength=10, dom="tip", columnDefs=list(list(
-                    targets=2,
-                    render=JS(
-                      "function(data, type, row, meta) {",
-                      "return '<span title=\"' + data + '\"><a href=\"https://www.google.com/search?q=' + data + ' video game\">search for this game</a></span>';",
-                      "}")
-                  ))))
+                  options=list(pageLength=10, dom="tip", columnDefs=list(
+                    list(targets=3, #Transform the 3rd column into a link
+                         render=JS(
+                           "function(data, type, row, meta) {",
+                           "return '<span title=\"' + data + '\"><a href=\"https://www.google.com/search?q=' + data + ' video game\">search for this game</a></span>';",
+                           "}")
+                    )
+                  )))
   })
-  
+
   #Target title 
   output$targetTitle <- DT::renderDataTable({
     DT::datatable(targetTitleTb.out() %>%
                     select(-V1) %>%
-                    mutate_at(vars(matches("^[1-9]+$")), funs(percent)), #Change into percent format
+                    mutate_at(vars(matches("^.+[1-9]+$")), funs(percent)), #Change into percent format
                   selection="none",
-                  options=list(pageLength=1, dom="t"))
+                  options=list(pageLength=1, dom="t", columnDefs=list(
+                    list(targets="_all", #Center text in all column
+                         className="dt-center")
+                  )))
   })
   
   #Similar title 
   output$similarTitle <- DT::renderDataTable({
     DT::datatable(similarTitleTb.out() %>%
                     select(-V1) %>%
-                    mutate_at(vars(matches("^[1-9]+$")), funs(percent)), #Change into percent format
+                    mutate_at(vars(matches("^.+[1-9]+$")), funs(percent)), #Change into percent format
                   selection="none",
-                  options=list(pageLength=5, dom="t"))
+                  options=list(pageLength=5, dom="t", columnDefs=list(
+                    list(targets="_all", #Center text in all column
+                         className="dt-center")
+                  )))
+  })
+  
+  #Dominant features
+  output$dominantFeature <- DT::renderDataTable({
+    DT::datatable(dominantKeyTb.out(),
+                  selection="none",
+                  options=list(pageLength=5, dom="t", columnDefs=list(
+                    list(targets=c(1, 2), #Center text in column 1 and 2
+                         className="dt-center"),
+                    list(targets=3, #Make keyword text smaller
+                         className="s-text")
+                  )))
   })
   
   #Distinguishing strong features
   output$dStrongFeature <- DT::renderDataTable({
     DT::datatable(distingushingKeyTb.out() %>%
-                    filter(V1 > 0) %>%
-                    select(-V1, -Abs) %>%
-                    mutate(Percentage = percent(Percentage)), #Change into percent format
+                    filter(Similarity > 0) %>%
+                    select(-Similarity, -Similarity.abs) %>%
+                    mutate(`Percent Rank` = percent(`Percent Rank`)), #Change into percent format
                   selection="none",
-                  options=list(pageLength=5, dom="t"))
+                  options=list(pageLength=5, dom="t", columnDefs=list(
+                    list(targets=c(1, 2), #Center text in column 1 and 2
+                         className="dt-center"),
+                    list(targets=3, #Make keyword text smaller
+                         className="s-text")
+                  )))
   })
   
-  #Distinguishing strong features
+  #Distinguishing weak features
   output$dWeakFeature <- DT::renderDataTable({
     DT::datatable(distingushingKeyTb.out() %>%
-                    filter(V1 < 0) %>%
-                    select(-V1, -Abs) %>%
-                    mutate(Percentage = percent(Percentage)), #Change into percent format
+                    filter(Similarity < 0) %>%
+                    select(-Similarity, -Similarity.abs) %>%
+                    mutate(`Percent Rank` = percent(`Percent Rank`)), #Change into percent format
                   selection="none",
-                  options=list(pageLength=5, dom="t"))
+                  options=list(pageLength=5, dom="t", columnDefs=list(
+                    list(targets=c(1, 2), #Center text in column 1 and 2
+                         className="dt-center"),
+                    list(targets=3, #Make keyword text smaller
+                         className="s-text")
+                  )))
   })
-
-  
-  #--Headers
-  #Main panel
-  output$mainHeader_1 <- renderUI({
-    HTML("<h3>Target Game</h3>")
-  })
-  output$mainHeader_2 <- renderUI({
-    HTML("<hr><br> <h3>Similar Games</h3>")
-  })
-  output$mainHeader_3 <- renderUI({
-    HTML("<hr><br> <h3>Dominant Features</h3>")
-  })
-  output$mainHeader_4 <- renderUI({
-    HTML("<hr><br> <h3>Distingusishing Features</h3>")
-  })
-  output$mainHeader_4_1 <- renderUI({
-    HTML("<h4>Strong</h4>")
-  })
-  output$mainHeader_4_2 <- renderUI({
-    HTML("<br> <h4>Weak</h4>")
-  })
-  
 }
--- report/shiny/ui.R
+++ report/shiny/ui.R
@@ -27,13 +27,20 @@ ui <- fluidPage(#--Header
                 title='Game Experiential Topic',
                 
                 #Displayed title
-                titlePanel(h1("Game Experiential Topic Comparison")),   
+                titlePanel(HTML('<h3>Game Experiential Topic Comparison</h3>
+                                <p id="authorship">Chih-Yu Chiang • chihyuchiang@uchicago.edu</p>')),
                 
                 
-                #--Decide layout
+                #--Decide primary layout
                 sidebarLayout(
                   #Side bar                
-                    sidebarPanel(
+                  sidebarPanel(
+                    h3("Instruction"),
+                    p("(Please give the app 10 seconds to initialize)"),
+                    p("The table is not ordered by default (DataTables orders a table by its first column by default);
+                      Ordered columns are not highlighted by default (the DataTables option orderClasses is changed from TRUE to FALSE);
+                      Numeric columns are always aligned to the right, since it rarely makes sense for numbers to be aligned to the left;
+                      The option autoWidth is set to FALSE by default, so that DataTables does not calculate and put hard-coded width values on the table columns;"),
                     textInput(inputId="searchText", label="", value=""),
                     actionButton(inputId="searchButton", label="search"),
                     DT::dataTableOutput(outputId="searchResult")
@@ -41,17 +48,21 @@ ui <- fluidPage(#--Header
                   
                   #Main panel
                   mainPanel(
-                    htmlOutput(outputId="mainHeader_1"),
+                    h3("Target Title"),
                     DT::dataTableOutput(outputId="targetTitle"),
-                    htmlOutput(outputId="mainHeader_2"),                    
+                    br(), hr(),
+                    h3("Similar Titles"),                    
                     DT::dataTableOutput(outputId="similarTitle"),
-                    htmlOutput(outputId="mainHeader_3"), 
-                    htmlOutput(outputId="mainHeader_4"), 
-                    htmlOutput(outputId="mainHeader_4_1"),
+                    br(), hr(),                   
+                    h3("Dominant Features"), 
+                    DT::dataTableOutput(outputId="dominantFeature"),
+                    br(), hr(),
+                    h3("Distingusishing Features"),
+                    h4("Strong"),
                     DT::dataTableOutput(outputId="dStrongFeature"),
-                    htmlOutput(outputId="mainHeader_4_2"),
+                    br(),
+                    h4("Weak"),
                     DT::dataTableOutput(outputId="dWeakFeature")
                   )
                 )
-
 )
